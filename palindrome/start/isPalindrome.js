185123539e308b03e4f5a1f6209ec2b9a7344fb1
joeldlawler <joellawler@gmail.com>
2024-09-07 13:39:15 -0500

    add start files

---
+++ palindrome/start/isPalindrome.js
@@ -0,0 +1,9 @@
+// Please write a javascript function that takes a sting and returns true if the string is a palindrome and returns false if it is not.
+function isPalindrome(str) {}
+
+// Example calls
+
+// console.log(isPalindrome("A man, a plan, a canal: Panama")); // true
+// console.log(isPalindrome("race a car")); // false
+// console.log(isPalindrome("Was it a car or a cat I saw?")); // true
+// console.log(isPalindrome("hello")); // false
